102d55a8037df45715a4bd8fe4b259b482eccb43
Udit Sahani <uditrsahani@gmail.com>
2024-06-02 14:57:15 +0700

    final version of the website

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import HeroBgAnimation from '../HeroBgAnimation'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/constants';
+
+const HeroSection = () => {
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>Hi, I am <br /> Udit Sahani</Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: 'Full Stack Developer',
+                                        autoStart: true,
+                                        loop: false,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>Welcome to my portfolio! I'm a recent Information Technology graduate with a 3.8 GPA and over two years of professional and internship experience. Explore my projects, skills, and projects, showcasing my expertise in full stack development, software development, problem-solving.</SubTitle>
+                        <ResumeButton href='https://docs.google.com/document/d/1bXD1hAGljYmZypFktDGm055F8DUHjsb8MKQ7hV5Jp9E/edit' target='display'>Check Resume</ResumeButton>
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+
+                        <Img src="https://videshguru.com/wp-content/uploads/2023/03/Founder_VG-218x300.jpg" alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection
